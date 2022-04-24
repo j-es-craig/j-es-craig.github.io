a97acd5e3b5b6ffcba3d1873a2138a456d54a808
Joshua Craig <44414371+j-es-craig@users.noreply.github.com>
2022-04-24 10:57:06 -0400

    text additions

--- scripts.js
+++ scripts.js
@@ -6,3 +6,7 @@ document.querySelectorAll('a[href^="#"]').forEach(ancor => {
         });
     });
 });
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
